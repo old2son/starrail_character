c6d442c701a62ad3ff4985d11214a747d8548326
CJY <13632362832@163.com>
2023-08-25 18:11:54 +0800

    index video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,34 @@
 <script>
     export let data;
     import Nav from '@src/routes/nav.svelte';
+    import videoHome from '$lib/videos/home.mp4';
+    import { onMount } from 'svelte';
     import { title } from '@src/stores.js';
     title.set('home page'); 
     const logged = typeof data.logged === 'boolean' ? data.logged : false;
+
+    /**
+	 * @type {HTMLCanvasElement}
+	 */
+    let canvas;
+
+    onMount(() => {
+        canvasSize();
+    });
+
+    const canvasSize = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
 </script>
 
 <Nav {logged}/>
+<svelte:window on:resize={canvasSize} />
+<canvas bind:this={canvas} />
+<!-- todo 首页 canvas 视频 -->
+<!-- <track kind="captions"> -->
+<video src={videoHome}></video>
+<style>
+
+</style>
