f4e81550bf308a070dbfdcad9734906f4678c487
cjy <13632362832@163.com>
2026-03-31 10:06:53 +0800

    🐎 ci(svelte.config): build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-static'; // +layout.js  设置 prerender 
 
 /** @type {import('@sveltejs/kit').Config} */
